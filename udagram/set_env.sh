7979b3979f1bd9f93bae3eb9efb62dcd47b9637b
johnsamir555 <jshsoft@gmail.com>
2023-02-01 21:56:44 +0200

    edit apiHost and more

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,12 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=12345
-export POSTGRES_HOST=mydbinstance.csxbuclmtj3c.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres12345
+export POSTGRES_HOST=database-2.ckfxb0b5gxqj.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=project_db
-export AWS_BUCKET=arn:aws:s3:::myawsbucket-75139724085
+export AWS_BUCKET=arn:aws:s3:::udagram-backet-123456789
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=hello-from-egypt
-export URL=http://localhost:8100
+export URL=http://udagram-backet-123456789.s3-website-us-east-1.amazonaws.com/
 export NODE_OPTIONS=--openssl-legacy-provider
