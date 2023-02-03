367f097a9ce88179b1160f326a530a07b51c6fff
johnsamir555 <jshsoft@gmail.com>
2023-02-03 09:25:31 +0200

    edit api deploy scripts

--- udagram/udagram-api/bin/deploy.sh
+++ udagram/udagram-api/bin/deploy.sh
@@ -1,6 +1,6 @@
 cd www
-eb init $EB_APP --platform node.js --region $AWS_DEFAULT_REGION --profile $AWS_PROFILE
+eb init $EB_APP --platform node.js --region $AWS_DEFAULT_REGION
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME POSTGRES_PASSWORD=$POSTGRES_PASSWORD POSTGRES_DB=$POSTGRES_DB POSTGRES_HOST=$POSTGRES_HOST AWS_REGION=$AWS_REGION AWS_PROFILE=$AWS_PROFILE AWS_BUCKET=$AWS_BUCKET JWT_SECRET=$JWT_SECRET -e $EB_ENV
 eb list
 eb use $EB_ENV
-eb deploy --artifact "Archive.zip" 
+eb deploy $EB_ENV --artifact "Archive.zip" 
