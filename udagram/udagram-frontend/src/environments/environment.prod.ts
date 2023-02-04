8f34cb1b40cf5dcb1695c2524f5b03baeff6ea0a
johnsamir555 <jshsoft@gmail.com>
2023-02-04 12:16:05 +0200

    fix some eb env issues

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-qmnpzduq.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api-development.eba-qmnpzduq.us-east-1.elasticbeanstalk.com/api/v0',
   // apiHost:'http://localhost:8080/api/v0'
 };
 
