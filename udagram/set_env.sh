a318b10378f2e2b1083625306af80bb5a82a5624
johnsamir555 <jshsoft@gmail.com>
2023-02-04 16:08:30 +0200

    fix eb environments issues

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=postgres12345
-export POSTGRES_HOST=database-2.ckfxb0b5gxqj.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.csco5quqszgy.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=project_db
-export AWS_BUCKET=arn:aws:s3:::udagram-backet-123456789
+export AWS_BUCKET=udagram-backet-123456789
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=hello-from-egypt
