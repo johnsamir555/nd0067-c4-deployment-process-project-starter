bfa97058b62a699bcb037d0a9769f5a11d38d588
johnsamir555 <jshsoft@gmail.com>
2023-02-05 12:43:11 +0200

    work on connection issues

--- udagram/udagram-api/src/config/config.ts
+++ udagram/udagram-api/src/config/config.ts
@@ -12,6 +12,7 @@ export const config = {
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
   aws_region: process.env.AWS_REGION,
+  db_port: parseInt(process.env.POSTGRES_PORT),
   aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
